00b959f08b3aed36ffd6f45f9929bbe212eaa8c3
Alphacode18 <skharban@purdue.edu>
2021-04-25 12:36:06 +0530

    Amplitude Integrated For Analytics

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { Authentication } from './navigation/_Export';
+import { events, initialize, track } from './Analytics';
 
 export default () => (
   <>

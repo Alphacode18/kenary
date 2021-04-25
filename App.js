4a779664e20da751e6396615931986ff31f29835
Alphacode18 <skharban@purdue.edu>
2021-04-25 18:31:48 +0530

    Forgot Password Integrated

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import { Authentication } from './navigation/_Export';
+import { Authentication, Authenticated } from './navigation/_Export';
+import Firebase from './config/Firebase';
 import { events, initialize, track } from './Analytics';
 
-export default () => (
-  <>
-    <IconRegistry icons={EvaIconsPack} />
-    <ApplicationProvider {...eva} theme={eva.light}>
-      <Authentication />
-    </ApplicationProvider>
-  </>
-);
+export default () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  Firebase.auth().onAuthStateChanged((user) => {
+    user ? setIsAuthenticated(true) : setIsAuthenticated(false);
+  });
+  return (
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva.light}>
+        {isAuthenticated ? <Authenticated /> : <Authentication />}
+      </ApplicationProvider>
+    </>
+  );
+};

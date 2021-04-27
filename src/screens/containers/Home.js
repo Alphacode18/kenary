cbe2f1567dc019bd13864307134a9e89e235c034
Alphacode18 <skharban@purdue.edu>
2021-04-27 14:03:47 +0530

    Home Page Components Concept Added

--- src/screens/containers/Home.js
+++ src/screens/containers/Home.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import { Layout, Text, ViewPager, Avatar } from '@ui-kitten/components';
+import {
+  SafeAreaView,
+  StyleSheet,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import { Layout, Text, ViewPager, Avatar, Card } from '@ui-kitten/components';
 
 export default Home = ({ navigation }) => {
   const [selectedIndex, setSelectedIndex] = useState(0);
@@ -8,14 +12,19 @@ export default Home = ({ navigation }) => {
     <>
       <Layout style={styles.layout}>
         <SafeAreaView>
-          <Layout style={styles.header}>
-            <Text category='h1'>Experience</Text>
-            <Avatar
-              source={require('../../../assets/Shreyas.jpg')}
-              style={{ marginRight: 20 }}
-              onPress={() => navigation.navigate('Profile')}
-            />
-          </Layout>
+          <TouchableWithoutFeedback
+            onPress={() => {
+              navigation.navigate('Profile');
+            }}
+          >
+            <Layout style={styles.header}>
+              <Text category='h1'>Experience</Text>
+              <Avatar
+                source={require('../../../assets/Shreyas.jpg')}
+                style={{ marginRight: 20 }}
+              />
+            </Layout>
+          </TouchableWithoutFeedback>
           {/* Replace with the user's location */}
           <Text category='h1' style={styles.hero}>
             West Lafayette
@@ -25,16 +34,48 @@ export default Home = ({ navigation }) => {
             onSelect={(index) => setSelectedIndex(index)}
             style={{ marginTop: 20 }}
           >
-            <Layout style={styles.tab} level='4'>
-              <Text category='h5'>USERS</Text>
+            <Layout style={styles.tab} level='1'>
+              <Card>
+                <Text>
+                  The Maldives, officially the Republic of Maldives, is a small
+                  country in South Asia, located in the Arabian Sea of the
+                  Indian Ocean. It lies southwest of Sri Lanka and India, about
+                  1,000 kilometres (620 mi) from the Asian continent
+                </Text>
+              </Card>
             </Layout>
-            <Layout style={styles.tab} level='4'>
-              <Text category='h5'>ORDERS</Text>
+            <Layout style={styles.tab} level='1'>
+              <Card>
+                <Text>
+                  The Maldives, officially the Republic of Maldives, is a small
+                  country in South Asia, located in the Arabian Sea of the
+                  Indian Ocean. It lies southwest of Sri Lanka and India, about
+                  1,000 kilometres (620 mi) from the Asian continent
+                </Text>
+              </Card>
             </Layout>
-            <Layout style={styles.tab} level='4'>
-              <Text category='h5'>TRANSACTIONS</Text>
+            <Layout style={styles.tab} level='1'>
+              <Card>
+                <Text>
+                  The Maldives, officially the Republic of Maldives, is a small
+                  country in South Asia, located in the Arabian Sea of the
+                  Indian Ocean. It lies southwest of Sri Lanka and India, about
+                  1,000 kilometres (620 mi) from the Asian continent
+                </Text>
+              </Card>
             </Layout>
           </ViewPager>
+          <Layout
+            style={{
+              marginHorizontal: 20,
+              marginTop: 20,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Text category='h6'>Experience</Text>
+            <Text category='h6'>See All</Text>
+          </Layout>
         </SafeAreaView>
       </Layout>
     </>
@@ -50,14 +91,13 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     marginTop: 10,
-    marginLeft: 20,
+    marginLeft: 10,
   },
   hero: {
     color: '#cc7722',
-    marginLeft: 20,
+    marginLeft: 10,
   },
   tab: {
-    height: 192,
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 10,

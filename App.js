6c2b8bd1cdaa441a6a7d0b8e782e287cc9328058
Alphacode18 <skharban@purdue.edu>
2021-05-18 22:12:58 +0530

    Kenary Splash Screen Added

--- App.js
+++ App.js
@@ -1,18 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { Authentication, Authenticated } from './navigation/_Export';
 import Firebase from './config/Firebase';
 import { initialize, track, events } from './Analytics';
+import * as SplashScreen from 'expo-splash-screen';
 
 initialize();
+SplashScreen.preventAutoHideAsync()
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
 
 export default () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  Firebase.auth().onAuthStateChanged((user) => {
-    user ? setIsAuthenticated(true) : setIsAuthenticated(false);
-  });
+  useEffect(() => {
+    setTimeout(async () => {
+      await SplashScreen.hideAsync();
+    }, 2000);
+    Firebase.auth().onAuthStateChanged((user) => {
+      user ? setIsAuthenticated(true) : setIsAuthenticated(false);
+    });
+  }, []);
   return (
     <>
       <IconRegistry icons={EvaIconsPack} />

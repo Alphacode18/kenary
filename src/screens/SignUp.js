7adda9978df2095b8732e0609cd5fe83dc78b8b0
Alphacode18 <skharban@purdue.edu>
2021-05-24 17:45:46 +0530

    Back Button For Auth Flow (WIP)

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -12,6 +12,7 @@ import Firebase, { db } from '../../config/Firebase';
 import { Formik, Field } from 'formik';
 import * as yup from 'yup';
 import ValidatedInput from './components/ValidatedInput';
+import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
 
 const signUpValidationSchema = yup.object().shape({
   name: yup
@@ -59,89 +60,103 @@ export default SignUp = ({ navigation }) => {
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <Layout style={styles.container}>
-        <Text
-          style={{
-            padding: 20,
-            marginTop: 50,
-            fontSize: 30,
-          }}
-        >
-          Ready For Adventures?
-        </Text>
-        <Formik
-          validationSchema={signUpValidationSchema}
-          initialValues={{
-            name: '',
-            email: '',
-            password: '',
-            confirmPassword: '',
-          }}
-          onSubmit={(values) => {
-            handleSignUp(values);
-          }}
-        >
-          {({ handleSubmit, isValid }) => (
-            <>
-              <Field
-                component={ValidatedInput}
-                name='name'
-                placeholder='Name'
-              />
-              <Field
-                component={ValidatedInput}
-                name='email'
-                placeholder='Email Address'
-                keyboardType='email-address'
-              />
-              <Field
-                component={ValidatedInput}
-                name='password'
-                placeholder='Password'
-              />
-              <Field
-                component={ValidatedInput}
-                name='confirmPassword'
-                placeholder='Confirm Password'
-              />
-              <Button
-                onPress={handleSubmit}
-                disabled={!isValid}
-                style={{
-                  width: '75%',
-                  backgroundColor: 'black',
-                  borderRadius: 30,
-                  borderColor: 'black',
-                  height: 50,
-                  marginTop: 20,
-                }}
-                appearance='outline'
-              >
-                {loading === false ? (
-                  <Text style={{ color: 'white' }}>Sign Up</Text>
-                ) : (
-                  <Spinner size='small' status={'basic'} />
-                )}
-              </Button>
-            </>
-          )}
-        </Formik>
-        <TouchableOpacity
-          style={{ color: 'white', marginTop: 40 }}
-          onPress={() => navigation.navigate('Login')}
-        >
-          <Text>
-            <Text>Already have an account? </Text>
-            <Text style={{ textDecorationLine: 'underline' }}>Login</Text>
+        <Layout style={styles.header}>
+          <TouchableWithoutFeedback onPress={() => navigation.goBack()}>
+            <FontAwesome5Icon name='arrow-left' size={25}>
+              <Text>{'\t  '}</Text>
+            </FontAwesome5Icon>
+          </TouchableWithoutFeedback>
+        </Layout>
+        <Layout style={styles.container}>
+          <Text
+            style={{
+              padding: 20,
+              marginTop: 50,
+              fontSize: 30,
+            }}
+          >
+            Ready For Adventures?
           </Text>
-        </TouchableOpacity>
+          <Formik
+            validationSchema={signUpValidationSchema}
+            initialValues={{
+              name: '',
+              email: '',
+              password: '',
+              confirmPassword: '',
+            }}
+            onSubmit={(values) => {
+              handleSignUp(values);
+            }}
+          >
+            {({ handleSubmit, isValid }) => (
+              <>
+                <Field
+                  component={ValidatedInput}
+                  name='name'
+                  placeholder='Name'
+                />
+                <Field
+                  component={ValidatedInput}
+                  name='email'
+                  placeholder='Email Address'
+                  keyboardType='email-address'
+                />
+                <Field
+                  component={ValidatedInput}
+                  name='password'
+                  placeholder='Password'
+                />
+                <Field
+                  component={ValidatedInput}
+                  name='confirmPassword'
+                  placeholder='Confirm Password'
+                />
+                <Button
+                  onPress={handleSubmit}
+                  disabled={!isValid}
+                  style={{
+                    width: '75%',
+                    backgroundColor: 'black',
+                    borderRadius: 30,
+                    borderColor: 'black',
+                    height: 50,
+                    marginTop: 20,
+                  }}
+                  appearance='outline'
+                >
+                  {loading === false ? (
+                    <Text style={{ color: 'white' }}>Sign Up</Text>
+                  ) : (
+                    <Spinner size='small' status={'basic'} />
+                  )}
+                </Button>
+              </>
+            )}
+          </Formik>
+          <TouchableOpacity
+            style={{ color: 'white', marginTop: 40 }}
+            onPress={() => navigation.navigate('Login')}
+          >
+            <Text>
+              <Text>Already have an account? </Text>
+              <Text style={{ textDecorationLine: 'underline' }}>Login</Text>
+            </Text>
+          </TouchableOpacity>
+        </Layout>
       </Layout>
     </TouchableWithoutFeedback>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  header: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container: {
+    flex: 3,
     alignItems: 'center',
     justifyContent: 'center',
     paddingBottom: 100,

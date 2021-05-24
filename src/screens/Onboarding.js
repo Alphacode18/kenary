418731d1591ab333c7fa490160db391832bcc4f3
Alphacode18 <skharban@purdue.edu>
2021-05-24 10:01:53 +0530

    Responsiveness Added To Onboarding Page | Code Clutter Cleared

--- src/screens/Onboarding.js
+++ src/screens/Onboarding.js
@@ -5,11 +5,8 @@ import {
   TouchableOpacity,
   Keyboard,
   ImageBackground,
-  Dimensions,
-  Image,
 } from 'react-native';
 import { Layout, Text, Button } from '@ui-kitten/components';
-const { height, width } = Dimensions.get('screen');
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
@@ -18,43 +15,17 @@ import {
 export default Onboarding = ({ navigation }) => {
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <Layout style={styles.container} level={'1'}>
+      <Layout style={styles.container}>
         <ImageBackground
           source={require('../../assets/background.png')}
-          style={{ width: width, height: height, resizeMode: 'cover' }}
+          style={styles.backgroundImage}
         >
           <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-            <Text
-              style={{
-                top: 60,
-                left: width - width / 6,
-                fontSize: hp('1.75%'),
-              }}
-            >
-              Login
-            </Text>
+            <Text style={styles.minorCallToAction}>Login</Text>
           </TouchableOpacity>
-          <Text
-            style={{
-              top: height - height / 3 - 10,
-              left: wp('12%'),
-              fontSize: hp('4%'),
-              width: '85%',
-              fontWeight: '400',
-            }}
-          >
-            Experiences that create memories.
-          </Text>
+          <Text style={styles.tagline}>Experiences that create memories.</Text>
           <Button
-            style={{
-              width: '75%',
-              top: height - height / 3.5 - 20,
-              left: width / 8,
-              backgroundColor: 'black',
-              borderRadius: 30,
-              borderColor: 'black',
-              height: 50,
-            }}
+            style={styles.majorCallToAction}
             onPress={() => navigation.navigate('SignUp')}
           >
             <Text style={{ color: 'white' }}>Sign Up</Text>
@@ -67,15 +38,33 @@ export default Onboarding = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 4,
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  inputBox: {
-    width: '85%',
-    margin: 0,
-    padding: 15,
-    fontSize: 16,
-    textAlign: 'center',
+  backgroundImage: {
+    width: wp('100%'),
+    height: hp('100%'),
+    resizeMode: 'cover',
+  },
+  minorCallToAction: {
+    top: hp('7.5%'),
+    left: wp('100%') - wp('100%') / 6,
+    fontSize: hp('1.75%'),
+  },
+  tagline: {
+    top: hp('100%') - hp('100%') / 3 - 10,
+    left: wp('10%'),
+    fontSize: hp('5%'),
+    width: wp('85%'),
+  },
+  majorCallToAction: {
+    width: wp('75%'),
+    top: hp('100%') - hp('100%') / 3.5 - 20,
+    left: wp('100%') / 8,
+    backgroundColor: 'black',
+    borderRadius: 30,
+    borderColor: 'black',
+    height: 50,
   },
 });

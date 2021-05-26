0e9caf13b5561a9734d86433a242d11b328130df
Alphacode18 <skharban@purdue.edu>
2021-05-26 19:48:51 +0530

    Amplitude Integration Started

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { Authentication, Authenticated } from './navigation/_Export';
+import { initialize, events, track } from './Analytics';
 import Firebase from './config/Firebase';
 import * as SplashScreen from 'expo-splash-screen';
 import * as Location from 'expo-location';
+
+initialize();
 
 SplashScreen.preventAutoHideAsync()
   .then()
@@ -14,11 +17,12 @@ SplashScreen.preventAutoHideAsync()
 export default () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   useEffect(() => {
+    track(events.APP_OPEN);
     Firebase.auth().onAuthStateChanged((user) => {
       user ? setIsAuthenticated(true) : setIsAuthenticated(false);
     });
     (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
+      await Location.requestForegroundPermissionsAsync();
     })();
     setTimeout(async () => {
       await SplashScreen.hideAsync();
